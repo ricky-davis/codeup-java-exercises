d94f358876c3cb1495b65d4c6ae26a34df56d31f
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-13 12:27:29 -0500

    Finished Syntax, Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
+//        {
+//            System.out.print("Hello");
+//            System.out.print(",");
+//            System.out.print(" World");
+//            System.out.println("!");
+//        }
+//        int myFavoriteNumber = 666;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Testing";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+        //int three = (int) o;
 
+//        int x = 4;
+////        x = x + 5;
+//        x+=5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *=x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        x/=y;
+        y-=x;
+        System.out.println(y);
 
-        {
-            System.out.print("Hello");
-            System.out.print(",");
-            System.out.print(" World");
-            System.out.println("!");
-        }
     }
 }
