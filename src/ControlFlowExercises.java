299f742654c6bb8ded6f9493179b49ead943c9f6
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-14 14:24:46 -0500

    Finished Do While

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,19 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while(i<=15){
+        long i = 0;
+        do{
             System.out.println(i);
-            i++;
-        }
+            i+=2;
+        }while(i<=100);
+        i=100;
+        do{
+            System.out.println(i);
+            i-=5;
+        } while(i>=-10);
+        i=2;
+        do{
+            System.out.println(i);
+            i=i*i;
+        }while(i<1000000);
     }
 }
