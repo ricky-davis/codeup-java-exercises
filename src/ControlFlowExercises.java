7c3657bdbad9c154fc91e672c0b9350b3d2cb492
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-14 14:30:58 -0500

    Finished FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        long i = 0;
-        do{
-            System.out.println(i);
-            i+=2;
-        }while(i<=100);
-        i=100;
-        do{
-            System.out.println(i);
-            i-=5;
-        } while(i>=-10);
-        i=2;
-        do{
-            System.out.println(i);
-            i=i*i;
-        }while(i<1000000);
+        for(int i=1;i<=100;i++){
+            String buffer="";
+            if(i%3==0)buffer+="Fizz";
+            if(i%5==0)buffer+="Buzz";
+            System.out.println(buffer==""?i:buffer);
+        }
     }
 }
