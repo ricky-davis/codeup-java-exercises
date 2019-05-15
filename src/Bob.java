c89d941049eb68ea4842b5ff8d96db3c62eca99b
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-15 12:00:40 -0500

    Finished BobBot

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("> ");
+            String input = sc.nextLine();
+
+            if(input.equals("")){
+                System.out.println("Bob: Fine. Be that way!");
+            }else if(input.substring(input.length()-1).equals("?")){
+                System.out.println("Bob: Sure.");
+            }else if(input.substring(input.length()-1).equals("!")){
+                System.out.println("Bob: Whoa, chill out!");
+            }else if (input.equals("quit")){
+                break;
+            }else{
+                System.out.println("Bob: Whatever.");
+            }
+        }
+    }
 }
