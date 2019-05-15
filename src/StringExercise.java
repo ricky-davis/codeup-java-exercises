242d917227ab3855de7fe8399bc2998472cd2687
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-15 11:54:44 -0500

    Finished StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String message="We don't need no ";
+        System.out.println(message + "education\n"+message + "thought control");
+
+        message="Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+
+        message="In windows, the main drive is usually C:\\";
+        System.out.println(message);
+
+        message="I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+    }
+}
