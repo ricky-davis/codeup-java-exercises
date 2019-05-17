9405ad5445f3d51267ca4227fdff675e29c551a7
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-17 11:31:00 -0500

    Finished part 2 of Person Objects exercise

--- src/Person.java
+++ src/Person.java
@@ -2,10 +2,19 @@ public class Person {
     private String name;
 
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.setName("John");
-        person1.sayHello();
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
