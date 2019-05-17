2f6d46015cb732bcf3f512cb9eac9e11c9e25509
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-05-17 12:02:52 -0500

    Finished Circle exercise

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,10 @@
+package shapes;
+import util.Input;
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        Circle circle = new Circle(input.getDouble());
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+    }
+}
